b10b5460cb8dfd3e70109609e17d9a01ffbaae78
Mateusz Widerski <mat.widerski@gmail.com>
2018-06-07 18:44:33 +0200

    Add file structure

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '913044147413-rdpn8dckqnamfdp90ukins2tr2m38ii1.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'MpLkvmnfKZkOwv5EQ-a2Y15B',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
